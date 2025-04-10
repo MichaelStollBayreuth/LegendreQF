23d674615f65356422e3c778af27a6e5177545e8
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2025-04-10 20:02:54 +0200

    bump to v4.18.0

--- lakefile.lean
+++ lakefile.lean
@@ -6,7 +6,7 @@ package «legendre_QF» {
 }
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.17.0"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.18.0"
 
 @[default_target]
 lean_lib «LegendreQF» {
